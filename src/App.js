83cb57d10a372af80a9445274be10fb27d36d78b
TaePoong719 <just5538@gmail.com>
2023-10-24 12:54:46 +0900

    초기설정 / 버튼 / 타이포그래피

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button'
+/* 아이콘 import 두가지 방법 */
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import AdjustOutlinedIcon from '@mui/icons-material/AdjustOutlined';
+import styled from '@emotion/styled'
+import { Typography } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button size="large" variant="contained"> contained</Button>
+      <Button endIcon={<AdjustOutlinedIcon/>} fullWidth variant="outlined"> outlined</Button>
+      <Button startIcon={<AcUnitIcon/>} variant="text"> text</Button>
+      <Button sx={{
+        backgroundColor: 'gray',
+        color:'white',
+        margin: '10px',
+        "&:hover":{
+          backgroundColor: 'black'
+        },
+        "&:disabled":{
+          backgroundColor: 'white',
+          color: 'black'
+        }
+      }}> Custom Style</Button>
+      <CustomButton>Custom Button Style</CustomButton>
+      <Typography variant="h1" component="div" gutterBottom> h1. Heading</Typography> 
     </div>
   );
 }
+
+const CustomButton = styled(Button)({
+  backgroundColor: 'gray',
+  color:'white',
+  margin: '10px',
+  "&:hover":{
+    backgroundColor: 'black'
+  },
+  "&:disabled":{
+    backgroundColor: 'white',
+    color: 'black'
+  }
+})
 
 export default App;
