fcd025fd588c53de28aa4999351daceddf446c09
TaePoong719 <just5538@gmail.com>
2023-10-25 15:59:54 +0900

    modal

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,22 +1,53 @@
 import { Edit } from '@mui/icons-material'
-import { Fab, IconButton, Tooltip } from '@mui/material'
-import React from 'react'
+import { Box, Fab, IconButton, Modal, Tooltip, Typography } from '@mui/material'
+import React, { useState } from 'react'
 
 const Create = () => {
+  const [open, setOpen] = useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
   return (
-    <Tooltip title="Create"
-      sx={{
-        position: 'fixed',
-        bottom: '20px',
-        right: '30px'
-      }}>
-      <IconButton>
-        <Fab color="secondary">
-          <Edit />
-        </Fab>
-      </IconButton>
-    </Tooltip>
+    <>
+      <Tooltip title="Create"
+        sx={{
+          position: 'fixed',
+          bottom: '20px',
+          right: '30px'
+        }}>
+        <IconButton>
+          <Fab color="secondary">
+            <Edit onClick={handleOpen} />
+          </Fab>
+        </IconButton>
+      </Tooltip>
+      <Modal
+        open={open}
+        onClose={handleClose}>
+          <Box sx={style}>
+            <Typography id="modal-title6" variant="h6">
+              Text in a modal 
+            </Typography>
+            <Typography id="modal-description" sx={{mt:2}}>
+              Text in a modal Text in a modal  Text in a modal  Text in a modal  Text in a modal 
+
+            </Typography>
+          </Box>
+      </Modal>
+    </>
   )
+}
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '400px',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
 }
 
 export default Create
